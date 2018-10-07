d2cb5ea0b7f8e0b4e4f1d29cfe88b9c0dec69bd8
Virginie Jovien <vjovien@yahoo.fr>
2018-10-07 10:49:03 +0200

    dev

--- assets/js/controle-formulaire.js
+++ assets/js/controle-formulaire.js
@@ -1,29 +1,61 @@
 'use strict';
-/*
-$(function(){
-    $("form").on("submit", function() {
-      if($("input").val().length < 4) {
-        $("div.form-group").addClass("has-error");
-        $("div.alert").show("slow").delay(4000).hide("slow");
-        return false;
-      }
+
+$(document).ready(function () {
+
+    $('#form-inscription').validate({ 
+        rules: {
+            nomInscrit: {
+                required: true,
+                minlength: 4,
+                maxlength: 20,
+                lettreAvecEspace: true
+            },
+            mailInscrit: {
+                required: true,
+                email: true
+            },
+            mpInscrit: {
+                required: true,
+                minlength: 6,
+                maxlength: 10,
+                Uppercase: true,
+                LowserCase: true,
+                Digit: true
+            },
+             mp2Inscrit: {
+                equalTo: "#mp-inscription"
+                }
+        }
     });
-  }); 
-*/
+
+    jQuery.validator.addMethod("lettreAvecEspace", function(value, element) { 
+        return this.optional(element) || /^[a-z][a-z\s]*$/i.test(value); 
+    }, "Lettres seulement"); 
+    $.validator.addMethod("Uppercase", function (value, element) {
+        return this.optional(element) || /[A-Z]/.test(value);
+    }, "Entrer au moins une lettre majuscule");
+    $.validator.addMethod("LowserCase", function (value, element) {
+        return this.optional(element) || /[a-z]/.test(value);
+    }, "Entrer au moins une lettre minuscule");
+    $.validator.addMethod("Digit", function (value, element) {
+        return this.optional(element) || /[0-9]/.test(value);
+    }, "Entrer au moins un nombre");   
+});
+
+/*
 $(document).ready(function(){
 	// Ajout de notre méthode
 	$.validator.addClassRules({
         checkinput:{
-           
-       
+            required:true,
             minlength: 5,
-            maxlength: 5
+            maxlength: 9
         },
         checkemail:{
-         
+            required:true,
             email: true
         }
     });
 	// Initialisation du plugin
 	$("#form-inscription").validate();
-});
+});*/
---
+++ assets/js/inscription.js
@@ -0,0 +1,25 @@
+'use strict';
+$(document).ready(function () {
+
+    $('#form-inscription').validate({ // initialize the plugin
+        rules: {
+            nomInscrit: {
+                required: true,
+                minlength: 4,
+                maxlength: 15
+            },
+            mailInscrit: {
+                required: true,
+                email: true
+            },
+            mpInscrit: "required",
+            mp2Inscrit: {
+            equalTo: "#mp-inscription"
+    }
+        }
+    });
+
+});/*
+jQuery . validateur . addMethod ( "extension" , fonction ( valeur , élément , param ) { 
+                param = typeof param === "chaîne" ? param . remplacer ( /, / g , '|' ) : "png | jpe? g | gif" ; retourner ce . en option ( élément ) || valeur . correspondance ( nouveau         
+                   RegExp ( ". (" + Param + ") $" , "i" )); }, jQuery . format ( "Veuillez entrer une valeur avec une extension valide." ));   */
